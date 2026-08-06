91733ed2eb12a97e2ac6fa0c85447b26a46c8304
Manitej Narayanadasu <mn7231f@gre.ac.uk>
2026-08-06 15:12:38 +0530

    Add files via upload

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        left = 0
+        need = {}
+        window = {}
+        have = 0
+        need_count = 0
+        min_length = float("inf")
+        start = 0
+        for i in t:
+            if i in need:
+                need[i] += 1
+            else:
+                need[i] = 1
+        for right in range(len(s)):
+            if s[right] not in window:
+                window[s[right]] = 1
+            else:
+                window[s[right]] += 1
+            need_count = len(need.keys())
+            if s[right] in need and window[s[right]] == need[s[right]]:
+                have += 1
+            while need_count == have:
+                length = right - left + 1
+                if length < min_length:
+                    min_length = length
+                    start = left
+                if s[left] in need:
+                    window[s[left]] -= 1
+                    if window[s[left]] < need[s[left]]:
+                        have -= 1
+                left += 1
+        if min_length == float("inf"):
+            return ""
+        return s[start:start + min_length]
